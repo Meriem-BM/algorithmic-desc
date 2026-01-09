eb5d7d04968dca652ec731283f714ff83d355e1c
Meriem-BM <barhoumi.meriem1@gmail.com>
2026-01-09 18:06:00 +0100

    test: add failed mint mock class

---
+++ test/mocks/MockFailedMintDSC.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockFailedMintDSC is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _;
+    }
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(
+        uint256 _amount
+    ) public override moreThanZero(_amount) onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner moreThanZero(_amount) returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        _mint(_to, _amount);
+        return false;
+    }
+}
