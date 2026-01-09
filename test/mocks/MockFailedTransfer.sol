da6dd1ed0c5b9892e3848365e77588a70aedfb2b
Meriem-BM <barhoumi.meriem1@gmail.com>
2026-01-09 18:06:22 +0100

    test: update failed mint mock contract

--- test/mocks/MockFailedTransfer.sol
+++ test/mocks/MockFailedTransfer.sol
@@ -12,21 +12,39 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract MockFailedTransfer is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__AmountMustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor() ERC20("MockFailedTransfer", "MFT") Ownable(msg.sender) {}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _;
+    }
 
     /**
      * @notice Mint tokens for testing purposes
      * @dev Useful for setting up test balances before testing transfer failures
      */
-    function mint(address account, uint256 amount) public {
-        _mint(account, amount);
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner moreThanZero(_amount) returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        _mint(_to, _amount);
+        return true;
     }
 
     /**
      * @notice Always returns false to simulate transfer failure
      */
-    function transfer(address /*recipient*/, uint256 /*amount*/) public pure override returns (bool) {
+    function transfer(
+        address /*recipient*/,
+        uint256 /*amount*/
+    ) public pure override returns (bool) {
         return false;
     }
 
@@ -35,15 +53,10 @@ contract MockFailedTransfer is ERC20Burnable, Ownable {
      * @dev This is critical for testing depositCollateral which uses transferFrom
      */
     function transferFrom(
-        address, /*sender*/
-        address, /*recipient*/
+        address /*sender*/,
+        address /*recipient*/,
         uint256 /*amount*/
-    )
-        public
-        pure
-        override
-        returns (bool)
-    {
+    ) public pure override returns (bool) {
         return false;
     }
 }
