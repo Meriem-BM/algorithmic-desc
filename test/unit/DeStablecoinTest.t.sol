3ea086a89acfd23df8680504185477db127e2dd8
Meriem-BM <barhoumi.meriem1@gmail.com>
2026-01-15 17:09:53 +0100

    test: add stablecoin contract unit test

---
+++ test/unit/DeStablecoinTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeStablecoin} from "../../src/DeStablecoin.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+
+contract DeStablecoinTest is StdCheats, Test {
+    DeStablecoin public deStablecoin;
+
+    function setUp() public {
+        deStablecoin = new DeStablecoin();
+    }
+
+    address public user = makeAddr("user");
+
+    function test_MustMintMoreThanZero() public {
+        vm.startPrank(deStablecoin.owner());
+        vm.expectRevert(DeStablecoin.DeStablecoin__AmountMustBeGreaterThanZero.selector);
+        deStablecoin.mint(user, 0);
+    }
+
+    function test_MustBurnMoreThanZero() public {
+        vm.startPrank(deStablecoin.owner());
+        deStablecoin.mint(user, 100);
+        vm.expectRevert(DeStablecoin.DeStablecoin__AmountMustBeGreaterThanZero.selector);
+        deStablecoin.burn(0);
+        vm.stopPrank();
+    }
+
+    function test_CannotBurnMoreThanBalance() public {
+        vm.startPrank(deStablecoin.owner());
+        deStablecoin.mint(user, 100);
+        vm.expectRevert();
+        deStablecoin.burn(101);
+        vm.stopPrank();
+    }
+
+    function test_CantMintToZeroAddress() public {
+        vm.startPrank(deStablecoin.owner());
+        vm.expectRevert(DeStablecoin.DeStablecoin__InvalidAddress.selector);
+        deStablecoin.mint(address(0), 100);
+        vm.stopPrank();
+    }
+}
